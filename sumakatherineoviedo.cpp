60334bc95eb0f74da4d6c34ef75b007e3e7c5c61
Kattyro <145017209+Kattyro@users.noreply.github.com>
2023-09-14 21:32:37 -0500

    Add files via upload

---
+++ sumakatherineoviedo.cpp
@@ -0,0 +1,18 @@
+/*programa:suma.cpp*/
+/*autor: katherine oviedo*/
+/*fecha: 2023-09-14*/
+/*Descripcion: sumas dos numeros*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,z;
+	cout<<"Ingrese x:";cin>>x;
+		cout<<"Ingrese y:";cin>>y;
+	//Aqui se realiza la suma//
+	z=x+y;
+	cout<<"La suma de"<<x<<"+"<<y<<"="<<z<<endl;
+	return(0);
+}
+
+
